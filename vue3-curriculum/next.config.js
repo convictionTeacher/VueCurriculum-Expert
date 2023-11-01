cf7af593e5562eeeb36b8700b22099a042deb09d
ConvictionTeacher <conviction.teacher@gmail.com>
2023-11-01 23:55:41 +0900

    run build fix env

--- vue3-curriculum/next.config.js
+++ vue3-curriculum/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NOTION_TOKEN: 'secret_ATdKADQsT14BkKfD7yZoKF4jnd4PBh4UQ30EkhIl1DN',
+    NOTION_DATABASE_ID: 'e75061feeca144ea95cd2af10c880ff8'
+  }
 }
 
 module.exports = nextConfig
